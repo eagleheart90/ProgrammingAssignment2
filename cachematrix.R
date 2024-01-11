f71781838b5d0ed40ed7dc9ade628b00e73b6c60
eagleheart90 <dbmitchell90@gmail.com>
2024-01-11 17:47:14 -0500

    Assignment Completion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+#Set input x as a matrix
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        z <- NULL
+        set <- function(y){
+            x<<- y
+            z<<- NULL
+        }
+        get<-- function() x
+        setsolve <- function(solve) z <<- solve
+        getsolve<- function() z
+        list(set=set, get=get,
+             setsolve = setsolve,
+             getsolve= getsolve)
 }
 
 
-## Write a short comment describing this function
+##Change mean to solve and m to z
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        z <- x$getsolve()
+        if(!is.null(z)){
+            message("retrieving inversed matrix")
+            return(z)
+        }
+        data <- x$get()
+        z <- solve(data, ...)
+        x$setsolve(z)
+        z
+        
+        
 }
